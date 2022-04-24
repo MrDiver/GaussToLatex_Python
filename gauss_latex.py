37ca49e0e6c9cee7a48b38078c6dd51d3a9e22ce
Tristan Schulz <mrdiverlp@gmail.com>
2022-04-24 13:59:21 +0200

    Update gauss_latex.py

--- gauss_latex.py
+++ gauss_latex.py
@@ -98,34 +98,3 @@ class GaussSolution:
         s += "&"+self._get_mat_latex(self.mat) + "\n"
         s += "\\end{align*}"
         return s
-    
-    
-    
-    
-
-def main():
-    mat = np.array([[ 2,  5,  7, 1, 0 ,0],
-                    [ 6,  3,  4, 0, 1, 0],
-                    [ 5, -2, -3, 0, 0, 1]])
-        
-    solver = GaussSolution(mat, 4)
-    solver.add(0,2)
-    solver.add(0,1,-1)
-    solver.add(1,2,-1)
-    solver.add(1,0,-1)
-    solver.add(2,0,-5)
-    solver.add(1,2,2)
-    solver.add(2,1,2)
-    solver.add(1,2)
-    solver.mult(2,-1)
-    # print(solver.mat)
-    # print(solver.get_mat())
-    # print(solver.get_solution())
-    # for cmd in solver.command_history:
-        # print(cmd)
-    
-    print(solver.get_latex())
-    # print(solver.get_latex_preamble())
-
-if __name__ == "__main__":
-    main()
